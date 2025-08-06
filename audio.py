fca1522ef88894bd457016ae51872a7992d3fc89
amongsusman <149024306+amongsusman@users.noreply.github.com>
2025-08-06 14:54:44 +0200

    Update audio.py

--- audio.py
+++ audio.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
-hostile_mobs = ['blaze','bogged','breeze','creeper','elder guardian','ender dragon','endermite','evoker','ghast','guardian','hoglin','husk','magma cube','phantom','piglin brute','pillager','ravager','shulker','silverfish','skeleton','skeleton horseman','slime','spider jockey','stray','vex','vindicator','warden','witch', 'which','wither','wither skeleton','zoglin','zombie','zombie villager', 'bee', 'cave spider', 'dolphin', 'drowned', 'enderman', 'goat', 'iron golem', 'llama', 'piglin', 'panda', 'polar bear', 'wolf', 'zombified piglin']
+hostile_mobs = ['blaze','bogged','breeze','creeper','elder guardian','ender dragon','endermite','evoker','ghast', 'gassed','guardian','hoglin','husk','magma cube','phantom','piglin brute','pillager','ravager','shulker','silverfish','skeleton','skeleton horseman','slime','spider jockey','stray','vex','vindicator','warden','witch', 'which','wither','wither skeleton','zoglin','zombie','zombie villager', 'bee', 'cave spider', 'dolphin', 'drowned', 'enderman', 'goat', 'iron golem', 'llama', 'piglin', 'panda', 'polar bear', 'wolf', 'zombified piglin']
 
 FILE_PATH = 'C:\\Users\\tylep\\Downloads\\MinecraftServer2\\signal.txt' 
 
@@ -13,24 +13,27 @@ while True:
         audio = recognizer.listen(source)
     try:
         text = recognizer.recognize_google(audio).split()
-        #search for one word mobs
-        for i in range(len(text)):
-            if text[i].lower() in hostile_mobs:
+        for i in range(len(text) - 1):
+            word1, word2 = text[i], text[i + 1]
+            combined = word1 + " " + word2
+            if combined.lower() in hostile_mobs:
                 with open(FILE_PATH, "w") as file:
-                    if text[i].lower() == "which":
-                        file.write("WITCH")
-                    else:
-                        file.write(text[i].upper())
+                    file.write(combined.upper().replace(" ", "_"))
                 break
         else:
-            #search for two word mobs
-            for i in range(len(text) - 1):
-                word1, word2 = text[i], text[i + 1]
-                combined = word1 + " " + word2
-                if combined.lower() in hostile_mobs:
+            #search for one word mobs
+            for i in range(len(text)):
+                if text[i].lower() in hostile_mobs:
                     with open(FILE_PATH, "w") as file:
-                        file.write(combined.upper().replace(" ", "_"))
+                        if text[i].lower() == "which":
+                            file.write("WITCH")
+                        elif text[i].lower() == "gassed":
+                            file.write("GHAST")
+                        else:
+                            file.write(text[i].upper())
                     break
+        print(text)
+        
     except sr.UnknownValueError:
         print("Sorry, I could not understand what you said.")
     except sr.RequestError as e:
